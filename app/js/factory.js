1983933a54d151f74d1c510b1a644e467819c2ac
Nicola Turco <info@nekos.me>
2016-06-01 04:29:37 +0100

    changes to style and little addiction gulp update in order to remove comments when compiling angular modules

--- app/js/factory.js
+++ app/js/factory.js
@@ -1,12 +1,23 @@
+/**
+ * @name albumsData
+ * @desc Angular directive that communicates with the API
+ * @returns {Object}
+ */
+
 angular.module('app')
     .factory('albumsData', ['$http', function ($http) {
 
         var url = 'http://jsonplaceholder.typicode.com/photos';
         var albumsData = {};
 
+        /**
+         * @name getDetails
+         * @desc Method that returns the Array of object. Will be called in
+         *       Controller
+         * @returns {Object}
+         */
         albumsData.getDetails = function () {
             return $http.get(url);
         }
-
         return albumsData;
     }])
